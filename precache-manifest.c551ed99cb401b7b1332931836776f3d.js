334ca6389db96c0a771cbcb82324d5996ba74153
Sumiya Ushiro <u.sumi0820@gmail.com>
2020-02-01 23:52:46 +0800

    Updates

---
+++ precache-manifest.c551ed99cb401b7b1332931836776f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881dbf8eb2f3886e8c479ed706a037e2",
+    "url": "/Calorie-Calculator/index.html"
+  },
+  {
+    "revision": "77cb32721741dd90d2a0",
+    "url": "/Calorie-Calculator/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5845e336d4b5b840ea14",
+    "url": "/Calorie-Calculator/static/js/2.e6a9d49f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Calorie-Calculator/static/js/2.e6a9d49f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77cb32721741dd90d2a0",
+    "url": "/Calorie-Calculator/static/js/main.1d2b35aa.chunk.js"
+  },
+  {
+    "revision": "4e80eba3d9d5dbaac07c",
+    "url": "/Calorie-Calculator/static/js/runtime-main.56036655.js"
+  }
+]);
